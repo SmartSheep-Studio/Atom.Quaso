3188c44850376c2daf893c57c67a465d036c54ea
LittleSheep <littlesheep.code@hotmail.com>
2023-08-19 16:23:23 +0800

    :lipstick: Optimize grid

--- packages/quaso-web/src/views/landing.vue
+++ packages/quaso-web/src/views/landing.vue
@@ -1,21 +1,27 @@
 <template>
   <div class="container">
-    <div class="px-4 md:px-8 lg:px-24 xl:px-48">
-      <div class="pt-12 px-8 flex items-center">
-        <div class="mt-1 pl-4">
-          <div class="text-4xl font-bold">Quaso</div>
-          <div class="text-lg">Know what's new in the world.</div>
+    <n-grid item-responsive responsive="screen" :x-gap="8" :y-gap="8">
+      <n-gi span="0 m:4 l:6"></n-gi>
+
+      <n-gi span="24 m:16 l:12">
+        <div class="pt-12 px-8 flex items-center">
+          <div class="mt-1 pl-4">
+            <div class="text-4xl font-bold">Quaso</div>
+            <div class="text-lg">Know what's new in the world.</div>
+          </div>
         </div>
-      </div>
 
-      <div class="pt-10 px-8">
-        <new-post @submitted="surfingInst.fetch()" />
-      </div>
+        <div class="pt-10 px-8">
+          <new-post @submitted="surfingInst.fetch()" />
+        </div>
 
-      <div class="pt-4 px-8">
-        <surfing ref="surfingInst" />
-      </div>
-    </div>
+        <div class="pt-4 px-8">
+          <surfing ref="surfingInst" />
+        </div>
+      </n-gi>
+
+      <n-gi span="0 m:4 l:6"></n-gi>
+    </n-grid>
   </div>
 </template>
 
